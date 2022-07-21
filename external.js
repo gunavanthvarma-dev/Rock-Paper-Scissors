7a4d4fd8c368efb74a0cf48eab4fc191fb0bbb63
gunavanthdev <gunavanthvarma.dev@gmail.com>
2022-07-21 16:24:51 +0530

    modified getComputerChoice() to add new logic in external.js

--- external.js
+++ external.js
@@ -1,8 +1,8 @@
 function getComputerChoice()
 {
-    let choice;
     
-    let i;
+    
+   /* let i;
     for(i=0;i<=10;i++)
     {
         if(i%2==0)
@@ -13,6 +13,22 @@ function getComputerChoice()
             choice = "SCISSORS";
         
     }
+    return choice;*/
+
+    /* the above logic not random */
+    
+    /*the following logic*/
+
+    let choice;
+    let temp = Math.floor(Math.random()*10);
+
+    if(temp%2==0 && temp!=0)
+      choice="ROCK";
+    else if(temp%3==0 && temp!=0)
+      choice="PAPER";
+    else if(temp==0 || (temp%2!=0 && temp%3!=0))
+      choice ="SCISSORS";
+
     return choice;
 }
 
